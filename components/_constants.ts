9aa1c424ed9f92e960344a73f209963b66f779ff
Adam Fields <afields@6river.com>
2023-02-12 20:04:43 -0500

    refactor!: move previous functionality to adamelliotfields/dotfiles
    BREAKING CHANGE:

--- components/_constants.ts
+++
@@ -1,9 +0,0 @@
-import { homedir } from "std/node/os.ts";
-
-export const CONFIG_PATH = "../config.yml" as const;
-
-export const PUBLIC_PATH = "../public" as const;
-
-export const HOME_PATH = homedir() as string;
-
-export const NVM_DIR = `${HOME_PATH}/.nvm` as const;
--- components/_types.ts
+++
@@ -1,8 +0,0 @@
-import { Endpoints } from "@octokit/types";
-
-export type Release =
-  Endpoints["GET /repos/{owner}/{repo}/releases/latest"]["response"]["data"];
-
-// deno-fmt-ignore
-export type Asset =
-  Endpoints["GET /repos/{owner}/{repo}/releases/assets/{asset_id}"]["response"]["data"];
